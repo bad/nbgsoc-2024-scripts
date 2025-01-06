cb52945c42379a8d9aa0487da541991c5c6f422d
Christoph Badura <bad@bsd.de>
2025-01-06 12:11:52 +0100

    run-build.sh: log the script invocation to run-build.sh.log

--- run-build.sh
+++ run-build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 [ $# -lt 2 ] && { echo "usage: $0 machine args..." 1>2; exit 1; }
-echo "$(basename $0)" "$@"
+scriptname="$(basename $0)"
+# log the invocation.  note the TAB
+echo "$(date +%FT%T)	${scriptname}" "$@" >> "${scriptname}.log"
 OBJ=${TD:-..}/obj
 NCPU=$(( $(nproc) + 2 ))
 MACHINE=$1; shift
